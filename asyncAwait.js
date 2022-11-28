0a802a972f9710f8c9b1e365ed030d8f3fb4527d
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2022-11-28 13:54:25 -0600

    2811221354

---
+++ asyncAwait.js
@@ -0,0 +1,39 @@
+async function hola(nombre) {
+    return new Promise(function(resolve,reject){
+        setTimeout(function () {
+            console.log('Hola, ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log('bla bla bla...');
+            resolve(nombre);
+            reject('Falla al hablar');
+        }, 1000);
+    });
+}
+
+async function adios(nombre) {
+    return new Promise((resolve,reject) => {
+        setTimeout(function () {
+            console.log('Adios, ' + nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+async function main() {
+    let nombre = await hola('Carlos');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+}
+
+console.log('Inicia el proceso');
+main();
+console.log('Termina el proceso');
