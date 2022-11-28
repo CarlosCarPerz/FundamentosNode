d5e34da3c0b148d1a3200050dc1788e2dd0b0581
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2022-11-28 13:54:56 -0600

    zsesef

--- promises.js
+++ promises.js
@@ -1,20 +1,43 @@
-function hola(nombre, miCallback) {
-    setTimeout(function () {
-        console.log('Hola, ' + nombre);
-        miCallback(nombre);
-    }, 1000);
+function hola(nombre) {
+    return new Promise(function(resolve,reject){
+        setTimeout(function () {
+            console.log('Hola, ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    });
 }
 
-function hablar(callbackHablar) {
-    setTimeout(function() {
-        console.log('bla bla bla...');
-        callbackHablar();
-    }, 1000);
+function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log('bla bla bla...');
+            /* resolve(nombre); */
+            reject('Falla al hablar');
+        }, 1000);
+    });
 }
 
-function adios(nombre, otroCallback) {
-    setTimeout(function () {
-        console.log('Adios, ' + nombre);
-        otroCallback();
-    }, 1000);
+function adios(nombre) {
+    return new Promise((resolve,reject) => {
+        setTimeout(function () {
+            console.log('Adios, ' + nombre);
+            resolve();
+        }, 1000);
+    });
 }
+
+// ---
+
+console.log('iniciando el proceso')
+hola('Carlos')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Temrinado el proceso')
+    })
+    .catch(error => {
+        console.error('Ha habido un error: ' + error)
+    })
